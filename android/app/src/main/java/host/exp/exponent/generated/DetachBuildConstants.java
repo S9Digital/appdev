af87a1e6cda7fe12ba581383540c79e38eae21f9
Alex Atmore <acatmore@gmail.com>
2018-07-23 14:15:32 -0700

    changed gradle versions to support Ario provided libraries

--- android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -3,6 +3,6 @@ package host.exp.exponent.generated;
 // This file is auto-generated. Please don't rename!
 public class DetachBuildConstants {
 
-  public static final String DEVELOPMENT_URL = "";
+  public static final String DEVELOPMENT_URL = "exp898eec66984946c59e900317fc7ad0c5://10.0.0.156:80";
 
 }
--- components/clock.js
+++ components/clock.js
@@ -171,7 +171,7 @@ class Clock extends React.Component {
             marginTop: 30
           }}
         >
-          Set my sleep schedule
+          HI
         </Text>
       </BarContainer>
     );
