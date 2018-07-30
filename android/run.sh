89a43ab917ac76d15c1b12ab05608cee91afb646
Alex Atmore <acatmore@gmail.com>
2018-07-29 18:01:45 -0700

    detached version working

--- android/run.sh
+++ android/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 ./gradlew ${1:-installDevDebug} --stacktrace && adb shell am start -n host.exp.exponent/host.exp.exponent.LauncherActivity
+./gradlew ${1:-installDevMinSdkDevKernelDebug}
