8c6783723e84476514f5a1b69b36459b499eaec0
Eli Mallon <eli@stream.place>
2018-08-14 17:36:22 -0700

    Revert "remove client-specific file, .gitignore it"
    This reverts commit 185557cec9f26e32e09806ae2642897b0542842c.

---
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -0,0 +1,8 @@
+package host.exp.exponent.generated;
+
+// This file is auto-generated. Please don't rename!
+public class DetachBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "exp898eec66984946c59e900317fc7ad0c5://192.168.0.3:19000";
+
+}
