60475c43d8ff07f04b4de4c61adf5ade68b5fb44
Eli Mallon <eli@stream.place>
2018-09-05 13:27:04 -0700

    moved to monorepo

--- android/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-./gradlew ${1:-installDevDebug} --stacktrace && adb shell am start -n host.exp.exponent/host.exp.exponent.LauncherActivity
-./gradlew ${1:-installDevMinSdkDevKernelDebug}
